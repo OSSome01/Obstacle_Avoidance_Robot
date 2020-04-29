7a6d5529ba2ef3a8044362025a01f49a34fefd38
OSSome01 <omkarsargar01@gmail.com>
2020-04-29 20:39:34 +0530

    comments added

--- catkin_ws/src/motion_plan/scripts/obsatcle_avoidance.py
+++ catkin_ws/src/motion_plan/scripts/obsatcle_avoidance.py
@@ -4,7 +4,8 @@ import time
 from sensor_msgs.msg import geometry_msgs, LaserScan
 from geometry_msgs.msg import Twist
 
-def clbk_laser(msg):
+def callback_laser(msg): # callback function for subscriber
+    # divide total laser scan of 180 degrees into 5 parts
     regions = {
             'right':  min(min(msg.ranges[0:143]), 10),
             'fright': min(min(msg.ranges[144:287]), 10),
@@ -20,38 +21,38 @@ def avoid_obst(regions):
     angular_z = 0
     
     state_description = ''
-    
+    # check obstacle position and move accordingly
     if regions['front'] > 1 and regions['fleft'] > 1 and regions['fright'] > 1:
         state_description = 'case 1 - no obstacle in range'
         linear_x = 0.6
         angular_z = 0
     elif regions['front'] < 1 and regions['fleft'] > 1 and regions['fright'] > 1:
-        state_description = 'case 2 - front'
+        state_description = 'case 2 - obstacle position front'
         linear_x = 0
         angular_z = 0.3
     elif regions['front'] > 1 and regions['fleft'] > 1 and regions['fright'] < 1:
-        state_description = 'case 3 - fright'
+        state_description = 'case 3 - obstacle position fright'
         linear_x = 0
         angular_z = 0.3
     elif regions['front'] > 1 and regions['fleft'] < 1 and regions['fright'] > 1:
-        state_description = 'case 4 - fleft'
+        state_description = 'case 4 - obstacle position fleft'
         linear_x = 0
         angular_z = -0.3
     elif regions['front'] < 1 and regions['fleft'] > 1 and regions['fright'] < 1:
-        state_description = 'case 5 - front and fright'
+        state_description = 'case 5 - obstacle position front and fright'
         linear_x = 0
         angular_z = 0.3
     elif regions['front'] < 1 and regions['fleft'] < 1 and regions['fright'] > 1:
-        state_description = 'case 6 - front and fleft'
+        state_description = 'case 6 - obstacle position front and fleft'
         linear_x = 0
         angular_z = -0.3
     elif regions['front'] < 1 and regions['fleft'] < 1 and regions['fright'] < 1:
-        state_description = 'case 7 - front and fleft and fright'
+        state_description = 'case 7 - obstacle position front and fleft and fright'
         time.sleep(0.1)
         linear_x = 0
         angular_z = 0.3
     elif regions['front'] > 1 and regions['fleft'] < 1 and regions['fright'] < 1:
-        state_description = 'case 8 - fleft and fright'
+        state_description = 'case 8 - obstacle position fleft and fright'
         linear_x = 0.3
         angular_z = 0
     else:
@@ -67,7 +68,7 @@ def main():
     global pub
     rospy.init_node('reading_laser')
     pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
-    sub = rospy.Subscriber('obst_avoidance_robot/laser/scan', LaserScan, clbk_laser)
+    rospy.Subscriber('obst_avoidance_robot/laser/scan', LaserScan, callback_laser)
     rospy.spin()
 
 if __name__ == '__main__':
--- catkin_ws/src/motion_plan/scripts/reading_laser.py
+++ catkin_ws/src/motion_plan/scripts/reading_laser.py
@@ -2,8 +2,8 @@
 import rospy
 from sensor_msgs.msg import LaserScan
  
-def clbk_laser(msg):
-    # 720/5 = 144
+def callback_laser(msg): # callback function for subscriber
+    # divide total laser scan of 180 degrees into 5 parts
     regions = [ 
       min(msg.ranges[0:143],10),
       min(msg.ranges[144:287],10),
@@ -15,8 +15,7 @@ def clbk_laser(msg):
  
 def main():
     rospy.init_node('reading_laser')
-    sub = rospy.Subscriber("/obst_avoidance_robot/laser/scan", LaserScan, clbk_laser)
- 
+    rospy.Subscriber("/obst_avoidance_robot/laser/scan", LaserScan, callback_laser)
     rospy.spin()
  
 if __name__ == '__main__':
